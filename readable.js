148bdfff817b02fbf01bb46075d6c08cd0c012b2
Matthew Ferea <matthewferea@Matthews-MacBook-Pro-2.local>
2020-10-29 10:58:02 -0700

    Transpile generators

--- readable.js
+++ readable.js
@@ -4,13 +4,13 @@ if (process.env.READABLE_STREAM === 'disable' && Stream) {
   Object.assign(module.exports, Stream);
   module.exports.Stream = Stream;
 } else {
-  exports = module.exports = require('./lib/_stream_readable.js');
+  exports = module.exports = require('./build/_stream_readable.js');
   exports.Stream = Stream || exports;
   exports.Readable = exports;
-  exports.Writable = require('./lib/_stream_writable.js');
-  exports.Duplex = require('./lib/_stream_duplex.js');
-  exports.Transform = require('./lib/_stream_transform.js');
-  exports.PassThrough = require('./lib/_stream_passthrough.js');
-  exports.finished = require('./lib/internal/streams/end-of-stream.js');
-  exports.pipeline = require('./lib/internal/streams/pipeline.js');
+  exports.Writable = require('./build/_stream_writable.js');
+  exports.Duplex = require('./build/_stream_duplex.js');
+  exports.Transform = require('./build/_stream_transform.js');
+  exports.PassThrough = require('./build/_stream_passthrough.js');
+  exports.finished = require('./build/internal/streams/end-of-stream.js');
+  exports.pipeline = require('./build/internal/streams/pipeline.js');
 }
